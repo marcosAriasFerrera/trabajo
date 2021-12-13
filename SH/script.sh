ce3d6569201d941cecf79c081aeefa8c1da5e6b5
marcosAriasFerrera <48494310+marcosAriasFerrera@users.noreply.github.com>
2021-12-13 20:10:16 +0100

    Create script.sh

---
+++ SH/script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# SCRIPT DE DESPLIEGUE DE PROYECTO
+#
+# Jose Javier Perez Suarez
+#
+
+# Variables de entorno
+
+USERDB="debianDB"
+PASSDB="debianDB"
+HOST=$(hostname -I)
+WWW="/var/www/html/"
+# Nombre del fichero de los datos en el proyecto 
+DATOS="DBjardin.sql"
+BBDD="jardin"
+
+# Se toman los parámetros como USER y PASS de la BBDD
+if [ $# = 2 ];
+then
+   USERDB=$1
+   PASSDB=$2
+fi
+
+# Copiamos el contenido de la carpeta proyecto a la página html
+cp -r ../Codigo/jardin $WWW
+
+# Restauramos los datos de ejemplo a la BBDD
+mysqladmin -u $USERDB -p$USERDB create $BBDD
+mysql -u $USERDB -p$USERDB $BBDD < ../BD/$DATOS
+
+# Mostramos url de carga
+echo "http://$HOST/Codigo/Index.php"
